109218ab5a207ad66de12b997168e773a161b8c7
Danilo Montecel <daniloomarmontecel@gmail.com>
2025-01-05 14:02:50 -0500

    Corrigiendo

--- src/component/Galery.jsx
+++ src/component/Galery.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import Modal from "./Modal";
 import './Galery.css'
 const images = [
-    {id:1, src:'../../public/images/imgen1.jpg', alt:'images 1'},
-    {id:2, src:'../../public/images/image2.jpg',alt:'images 2'},
-    {id:3, src:'../../public/images/image3.jpg', alt:'images 3'},
-    {id:4, src:'../../public/images/image4.jpg',alt:'images 4'}
+    {id:1, src:'../../images/imgen1.jpg', alt:'images 1'},
+    {id:2, src:'../../images/image2.jpg',alt:'images 2'},
+    {id:3, src:'../../images/image3.jpg', alt:'images 3'},
+    {id:4, src:'../../images/image4.jpg',alt:'images 4'}
 ]
 
 const Galery = () => {
